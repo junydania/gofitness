cdaa551a3a96256a2309aa9086783fc4abb15a52
Osegbemoh Dania <o.dania@aol.com>
2018-05-21 00:22:38 +0100

    Redeployed application with nginx and puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock "~> 3.10.2"
 
 set :application, "gofitness"
-
 set :repo_url, 'https://github.com/junydania/gofitness.git'
-
 set :use_sudo, true
-
-set :deploy_to, "/home/deployer/apps/gofitness"
-
+set :user,            'railsdeploy'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+set :deploy_to, "/home/railsdeploy/apps/gofitness"
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :pty, true
 
 set :format, :pretty
