ce91eab43528fcaf26a06f096f4c9580abbf8ba4
Osegbemoh Dania <o.dania@aol.com>
2018-05-30 16:34:48 +0100

    fixed validation problem

--- lib/capistrano/tasks/seed.rb
+++ lib/capistrano/tasks/seed.rb
@@ -1,19 +1,19 @@
-user = User.create!(
-    [{
-         email: 'dominic@gofitness.com',
-         password: '12345678',
-         password_confirmation: '12345678',
-         first_name: 'Dominic',
-         last_name: 'Mudabai',
-         role: 0
-     }, {
-         email: 'o.dania@aol.com',
-         password: '12345678',
-         password_confirmation: '12345678',
-         first_name: 'Osegbemoh',
-         last_name: 'Dania',
-         role: 0
-     }]
-)
+# user = User.create!(
+#     [{
+#          email: 'dominic@gofitness.com',
+#          password: '12345678',
+#          password_confirmation: '12345678',
+#          first_name: 'Dominic',
+#          last_name: 'Mudabai',
+#          role: 0
+#      }, {
+#          email: 'o.dania@aol.com',
+#          password: '12345678',
+#          password_confirmation: '12345678',
+#          first_name: 'Osegbemoh',
+#          last_name: 'Dania',
+#          role: 0
+#      }]
+# )
 
 
