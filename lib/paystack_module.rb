a2e371a177d958085b1a4b7a1d9a0f23bc421b50
Dania <o.dania@icloud.com>
2018-07-06 02:24:28 +0100

    changed the name of d env variables

--- lib/paystack_module.rb
+++ lib/paystack_module.rb
@@ -1,8 +1,8 @@
 module GoFitPaystack
 
     def self.instantiate_paystack
-        public_key = ENV["PAYSTACK_TEST_PUBLIC"]
-        secret_key = ENV["PAYSTACK_TEST_SECRET"]
+        public_key = ENV["PAYSTACK_PUBLIC_KEY"]
+        secret_key = ENV["PAYSTACK_SECRET_KEY"]
         paystackObj = Paystack.new(public_key, secret_key)
         return paystackObj
     end
