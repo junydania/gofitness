e2c33f95d716d139bbdb442d994477ccad48050c
Dania <o.dania@icloud.com>
2018-07-08 00:46:22 +0100

    refactored paystack payment module

--- lib/paystack_module.rb
+++ lib/paystack_module.rb
@@ -2,9 +2,8 @@ module GoFitPaystack
 
     def self.instantiate_paystack
         public_key = ENV["PAYSTACK_PUBLIC_KEY"]
-        secret_key = ENV["PAYSTACK_SECRET_KEY"]
-        paystackObj = Paystack.new(public_key, secret_key)
+        secret_key = ENV["PAYSTACK_PRIVATE_KEY"]
+        paystackObj = Paystack.new
         return paystackObj
-    end
-    
+    end    
 end
