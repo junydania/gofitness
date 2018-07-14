7cd8ff3c55147b0d2582eac47e048d974206bea3
Dania <o.dania@icloud.com>
2018-07-14 13:45:20 +0100

    deploy file update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,13 @@
 set :rails_env, "production"  # tell cap to run migrations using production env
 
-role :app, %w{gofitnessadmin@178.128.181.200}
+# role :app, %w{gofitnessadmin@178.128.181.200}
 # role :web, %w{gofitnessadmin@178.128.181.200}
 # role :db,  %w{gofitnessadmin@178.128.181.200}
 
 set :migration_role, :app
 
 # Defaults to the primary :db server
-set :migration_servers, -> { primary(fetch(:migration_role)) }
+# set :migration_servers, -> { primary(fetch(:migration_role)) }
 
 # Define server(s)
 server '178.128.181.200', user: 'gofitnessadmin', roles: %w{web app db}, port: 7872, primary: true
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,14 +1,14 @@
 set :rails_env, "staging"  # tell cap to run migrations using staging env
 
-role :app, %w{gofitnessadmin@35.196.34.34}
+# role :app, %w{gofitnessadmin@35.196.34.34}
 # role :web, %w{gofitnessadmin@35.196.34.34}
 # role :db,  %w{gofitnessadmin@35.196.34.34}
 
 set :migration_role, :app
 
 # Defaults to the primary :db server
-set :migration_servers, -> { primary(fetch(:migration_role)) }
+# set :migration_servers, -> { primary(fetch(:migration_role)) }
 
 # Define server(s)
-server '35.196.34.34', user: 'gofitnessadmin', roles: %w{app}, port: 7872, primary: true
+server '35.196.34.34', user: 'gofitnessadmin', roles: %w{app web db}, port: 7872, primary: true
 
