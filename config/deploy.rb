67cc4f14827c8cdb52e99e2a575e2e527ecfb7a4
Dania <o.dania@icloud.com>
2018-09-29 15:42:11 +0100

    temporary fix for cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -120,23 +120,23 @@ namespace :deploy do
     
 end
   
-namespace :rails do
-  desc "Remote console"
-  task :console do
-    on roles(:app) do |h|
-      run_interactively "bundle exec rails console #{fetch(:rails_env)}", h.user
-    end
-  end
+# namespace :rails do
+#   desc "Remote console"
+#   task :console do
+#     on roles(:app) do |h|
+#       run_interactively "bundle exec rails console #{fetch(:rails_env)}", h.user
+#     end
+#   end
 
-  desc "Remote dbconsole"
-  task :dbconsole do
-    on roles(:app) do |h|
-      run_interactively "bundle exec rails dbconsole #{fetch(:rails_env)}", h.user
-    end
-  end
+#   desc "Remote dbconsole"
+#   task :dbconsole do
+#     on roles(:app) do |h|
+#       run_interactively "bundle exec rails dbconsole #{fetch(:rails_env)}", h.user
+#     end
+#   end
 
-  def run_interactively(command, user)
-    info "Running `#{command}` as #{user}@#{host}"
-    exec "bash --login -c 'cd #{fetch(:deploy_to)}/current && #{command}'"
-  end
+#   def run_interactively(command, user)
+#     info "Running `#{command}` as #{user}@#{host}"
+#     exec "bash --login -c 'cd #{fetch(:deploy_to)}/current && #{command}'"
+#   end
 end
