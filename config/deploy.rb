322a276585b838c04f2e4d2b28bc360627d6d4c2
Dania <o.dania@icloud.com>
2018-09-29 14:53:50 +0100

    created new commit file

--- config/deploy.rb
+++ config/deploy.rb
@@ -121,28 +121,47 @@ namespace :deploy do
 end
   
 namespace :rails do
-  desc "Open the rails console on each of the remote servers"
+  desc "script/console on a remote server"
   task :console do
-    on roles(:app) do |host| #does it for each host, bad.
-      rails_env = fetch(:stage)
-      exec ("ruby #{current_path}/script/rails console #{rails_env}")
+    rails_env = fetch(:rails_env, "production")
+    server = find_servers(:roles => [:app]).first
+    run_with_tty server, %W( script/console #{rails_env} )
+  end
+
+  desc "script/dbconsole on a remote server"
+  task :dbconsole do 
+    rails_env = fetch(:rails_env, "production")
+    server = find_servers(:roles => [:app]).first
+    run_with_tty server, %W( script/dbconsole #{rails_env} )
+  end
+
+  set :rake_cmd do
+    rails_env = fetch(:rails_env, "production")
+    "cd #{current_path} && rake RAILS_ENV=#{rails_env}"
+  end
+
+  # FIXME run on only one server?
+  desc "task=command runs rake 'command' on application servers"
+  task :rake, :roles => [:app] do
+    if ENV['task']
+      run "#{rake_cmd} #{ENV['task']}"
+    else
+      # FIXME use logger instead of warn?
+      warn "USAGE: cap rails:rake task=..."
     end
   end
 
-  desc "Open the rails dbconsole on each of the remote servers"
-  task :dbconsole do
-    on roles(:db) do |host| #does it for each host, bad.
-      rails_env = fetch(:stage)
-      exec("ruby #{current_path}/script/rails dbconsole #{rails_env}" )
-    end
+  def run_with_tty server, cmd
+    # looks like total pizdets
+    command = []
+    command += %W( ssh -t #{gateway} -l #{self[:gateway_user] || self[:user]} ) if self[:gateway]
+    command += %W( ssh -t )
+    command += %W( -p #{server.port}) if server.port
+    command += %W( -l #{user} #{server.host} )
+    command += %W( cd #{current_path} )
+    # have to escape this once if running via double ssh
+    command += [self[:gateway] ? '\&\&' : '&&']
+    command += Array(cmd)
+    system *command
   end
-
-  # def execute_interactively(command)
-  #   user = fetch(:user)
-  #   port = fetch(:port) || 7872
-  #   key  = fetch(:key)
-  #   host = fetch(:host)
-  #   exec "ssh -i #{key} #{user}@#{host} -p #{port} -t 'cd #{deploy_to}/current && #{command}'"
-  # end
-
 end
