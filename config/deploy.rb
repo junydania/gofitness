b484b402fe1bb0df75792305e2d8d864534e5fa1
Dania <o.dania@icloud.com>
2018-09-29 12:06:53 +0100

    seeds file updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,6 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-load 'lib/tasks/seed'
-
-
 set :application, "gofitness"
 set :repo_url, 'https://github.com/junydania/gofitness.git'
 set :use_sudo, true
