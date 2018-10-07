2c0e13b1dda4619f25c9d10db4c151c5e676651d
Dania <o.dania@icloud.com>
2018-10-07 11:45:23 +0100

    fixed redis conf

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,5 +3,9 @@ Sidekiq.configure_server do |config|
     if File.exists?(schedule_file)
         Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
     end
+    config.redis = { url: "redis://localhost:6379/12" }
 end
 
+Sidekiq.configure_client do |config|
+    config.redis = { url: "redis://localhost:6379/12" }
+end
